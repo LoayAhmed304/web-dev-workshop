fccce69a2d795cc03aeb6b7cbc05ef184ed03b0c
Loay Ahmed <loayahmed304@gmail.com>
2024-08-22 22:51:39 +0300

    :arrow_up: :arrow_down: Task III - Adjusted top-bar visibility according to the scrolling condition

--- Task III - Personal Portfolio II/script.js
+++ Task III - Personal Portfolio II/script.js
@@ -1,4 +1,5 @@
 const sections = document.querySelectorAll(".hidden-section");
+const topBar = document.querySelector(".top-bar");
 
 const options = {
   threshold: 0.1,
@@ -17,3 +18,17 @@ const observerCallback = (entries, observer) => {
 
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
+
+let lastScrollTop;
+window.addEventListener("scroll", function () {
+  let scrollTop = document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    // Scrolling down -> hide it
+    topBar.classList.add("hidden-top-bar");
+  } else {
+    // Scrolling up -> display it
+    topBar.classList.remove("hidden-top-bar");
+  }
+  lastScrollTop = scrollTop;
+});
