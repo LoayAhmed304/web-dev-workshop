28aa51a63f11a1e79e1e0793fd4cb5b0c4b56bcc
Loay Ahmed <loayahmed304@gmail.com>
2024-08-22 22:31:12 +0300

    :sparkles: Task III - Added sections transitions

--- Task III - Personal Portfolio II/script.js
+++ Task III - Personal Portfolio II/script.js
@@ -1,1 +1,19 @@
-console.log(2);
+const sections = document.querySelectorAll(".hidden-section");
+
+const options = {
+  threshold: 0.1,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("shown-section");
+    } else {
+      entry.target.classList.remove("shown-section");
+      entry.target.classList.add("hidden-section");
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, options);
+sections.forEach((section) => observer.observe(section));
