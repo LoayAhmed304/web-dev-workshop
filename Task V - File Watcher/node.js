3c957f14617c68cbf9fde6873a3ceb2e6930c671
Loay Ahmed <loayahmed304@gmail.com>
2024-08-29 21:45:08 +0300

    :sparkles: Task V - Added time display for the events

--- Task V - File Watcher/node.js
+++ Task V - File Watcher/node.js
@@ -3,33 +3,6 @@ const path = require('path');
 
 const folderName = './Folder';
 
-// fs.watch(folderName, { recursive: true }, (eventType, file) => {
-//   console.log(eventType, file);
-// });
-
-// const allFiles = new Set(fs.readdirSync(folderName));
-// fs.watch(folderName, { recursive: true }, (eventType, file) => {
-//   let event;
-//   if (file) {
-//     const fileExists = fs.existsSync(path.join(folderName, file));
-
-//     if (eventType === 'rename') {
-//       if (!fileExists && allFiles.has(file)) {
-//         event = 'removed';
-//         allFiles.delete(file);
-//       } else if (fileExists && !allFiles.has(file)) {
-//         event = 'created';
-//         allFiles.add(file);
-//       } else {
-//         event = 'renamed';
-//       }
-//     } else event = 'changed';
-//     console.log(`File ${file} has been ${event}.`);
-//   }
-// });
-// fs.watchFile(folderName, { persistent: true }, (cur, prev) => {
-//   console.log(`At: ${cur.mtime}`);
-// });
 let event;
 fs.watch(folderName, (eventType, fileName) => {
   if (fileName) {
@@ -47,4 +20,7 @@ fs.watch(folderName, (eventType, fileName) => {
       }
     });
   }
+});
+fs.watchFile(folderName, { persistent: true, interval: 500 }, (cur, prev) => {
+  console.log(`At: ${cur.mtime}`);
 });
